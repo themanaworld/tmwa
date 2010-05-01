4e1efcdd5e9be9de4647e0ff6a605df0908e16bb
MadCamel <madcamel@gmail.com>
2010-04-30 22:20:08 -0400

    Remove unused function declarations from tmw.h
    I had forgotten to do this when I removed the functions themselves.

--- src/map/tmw.h
+++ src/map/tmw.h
@@ -3,8 +3,6 @@
 #include "map.h"
 
 int  tmw_CheckChatSpam (struct map_session_data *sd, char *message);
-int  tmw_CheckTradeSpam (struct map_session_data *sd);
-int  tmw_CheckSitSpam (struct map_session_data *sd);
 int  tmw_ShorterStrlen (char *s1, char *s2);
 int  tmw_CheckChatLameness (struct map_session_data *sd, char *message);
 void tmw_GmHackMsg (const char *fmt, ...);
