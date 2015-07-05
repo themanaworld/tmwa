7a561ba98e5908d4887a15148487ee1800312e1f
mekolat <mekolat@users.noreply.github.com>
2015-07-05 12:59:52 -0400

    fix getmapusers

--- src/map/map.cpp
+++ src/map/map.cpp
@@ -183,7 +183,7 @@ int map_addblock(dumb_ptr<block_list> bl)
         if (bl->bl_next)
             bl->bl_next->bl_prev = bl;
         m->blocks.ref(x / BLOCK_SIZE, y / BLOCK_SIZE).normal = bl;
-        if (bl->bl_type == BL::PC && !bool(bl->is_player()->status.option & Opt0::HIDE))
+        if (bl->bl_type == BL::PC)
             m->users++;
     }
 
@@ -211,7 +211,7 @@ int map_delblock(dumb_ptr<block_list> bl)
         return 0;
     }
 
-    if (bl->bl_type == BL::PC  && !bool(bl->is_player()->status.option & Opt0::HIDE))
+    if (bl->bl_type == BL::PC)
         bl->bl_m->users--;
 
     if (bl->bl_next)
