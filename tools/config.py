53051b7cace8db3ec948349e0538e393e4af6860
Freeyorp <TheFreeYorp+git@gmail.com>
2024-06-04 14:15:47 +0000

    Very basic python3 support
    Extremely minimal, not touching any __future__ imports or such.
    
    For serious use, see specing's WIP PR at:
      https://git.themanaworld.org/legacy/tmwa/-/merge_requests/256

--- tools/config.py
+++ tools/config.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
 #   config.py - generator for config file parsers
--- tools/protocol.py
+++ tools/protocol.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
 #   protocol.py - generator for entire TMWA network protocol
