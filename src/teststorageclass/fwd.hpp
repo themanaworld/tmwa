8a6da3aae88d4976fca21138a4d0bcfc1a1a40af
Freeyorp <TheFreeYorp+git@gmail.com>
2024-06-19 12:58:58 +0000

    WIP: ODR warning investigation
    This attempts to establish an upper bound for an isolated test case, using
    `NotAccountEmail` instead of `AccountEmail`.
    
    The warning is not currently triggered by building this psuedo-server.

---
+++ src/teststorageclass/fwd.hpp
@@ -0,0 +1,36 @@
+#pragma once
+//    teststorageclass/fwd.hpp - list of type names for mmo lib
+//
+//    Copyright © 2014 Ben Longbons <b.r.longbons@gmail.com>
+//
+//    This file is part of The Mana World (Athena server)
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "../sanity.hpp"
+
+#include "../ints/fwd.hpp" // rank 1
+#include "../strings/fwd.hpp" // rank 1
+#include "../compat/fwd.hpp" // rank 2
+#include "../generic/fwd.hpp" // rank 3
+#include "../io/fwd.hpp" // rank 4
+#include "../net/fwd.hpp" // rank 5
+// mmo/fwd.hpp is rank 6
+
+
+namespace tmwa
+{
+// meh, add more when I feel like it
+class NotAccountEmail;
+} // namespace tmwa
---
+++ src/teststorageclass/main.cpp
@@ -0,0 +1,37 @@
+//    map/main.cpp - entry point to tmwa-teststorageclass
+//
+//    Copyright © 2013-2014 Ben Longbons <b.r.longbons@gmail.com>
+//
+//    This file is part of The Mana World (Athena server)
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "../high/core.hpp"
+
+#include "teststorageclass.hpp"
+
+#include "../poison.hpp"
+
+
+namespace tmwa
+{
+namespace map
+{
+} // namespace map
+} // namespace tmwa
+
+int main(int argc, char **argv)
+{
+    return tmwa_main(argc, argv);
+}
