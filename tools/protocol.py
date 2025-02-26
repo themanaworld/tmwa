88dfbd91ed22ab4ad98b2b9f0243f9d9faa9b49d
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2025-02-26 10:01:29 +0100

    tools/protocol.py: Avoid usage of deprecated pipes.quote
    The deprecated `pipes.quote` was removed in Python 3.13. Fortunately,
    the same functionality has been available as `shlex.quote` since Python
    3.3.

--- tools/protocol.py
+++ tools/protocol.py
@@ -21,9 +21,8 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import glob
-import itertools
 import os
-from pipes import quote
+from shlex import quote
 from posixpath import relpath
 from weakref import ref as wr
 
